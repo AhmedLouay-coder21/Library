4906e2329ca0ed576def2c3541b14ea87c3e1fe9
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-09-07 22:52:10 +0300

    feat(book): implement Book function (constructor)

--- Library.js
+++ Library.js
@@ -1,9 +1,16 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title,author) {
   // the constructor...
+  if(!new.target)
+  {
+    throw error("You must use the 'new' operator to use the constructor!");
+  }
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
 }
-
-function addBookToLibrary() {
+function addBookToLibrary(title,author) {
   // take params, create a book then store it in the array
+
 }
