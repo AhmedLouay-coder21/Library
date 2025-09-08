341b6e1ae4eef87bb03edbe87de8ab465ca594a2
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-09-08 18:42:18 +0300

    feat: add displayBookInfo() function for user

--- Library.js
+++ Library.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
-
-function Book(title,author) {
+const Books = document.getElementById("Books");
+function Book(title, author, numberOfPages, isRead) {
   // the constructor...
   if(!new.target)
   {
@@ -9,9 +9,36 @@ function Book(title,author) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.isRead = isRead;
 }
-function addBookToLibrary(title,author) {
+function addBookToLibrary(title, author, numberOfPages, isRead) {
   // take params, create a book then store it in the array
-  const book = new Book(title, author);
+  const book = new Book(title, author, numberOfPages, isRead);
   myLibrary.push(book);
+}
+
+addBookToLibrary("Oliver Twist","Charles Dickens","608", "Yes");
+addBookToLibrary("Test","Me","0", "No");
+console.log(myLibrary[0]);
+function displayBookInfo(i)
+{
+    const newLine = document.createElement("tr");
+    Books.appendChild(newLine);
+    const title = document.createElement("td");
+    const author = document.createElement("td");
+    const numberOfPages = document.createElement("td");
+    const isRead = document.createElement("td");
+    title.textContent = myLibrary[i].title;
+    author.textContent = myLibrary[i].author;
+    numberOfPages.textContent = myLibrary[i].numberOfPages;
+    isRead.textContent = myLibrary[i].isRead;
+    newLine.appendChild(title);
+    newLine.appendChild(author);
+    newLine.appendChild(numberOfPages);
+    newLine.appendChild(isRead);
+}
+for(let i = 0; i < myLibrary.length; i++)
+{
+  displayBookInfo(i);
 }
